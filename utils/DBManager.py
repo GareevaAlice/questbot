1c13c9a1a6bbab9b633803be827f35b538f21362
NanamyYu <lizashatskaja@mail.ru>
2022-12-03 13:52:51 +0300

    Update DBManager.py

--- utils/DBManager.py
+++ utils/DBManager.py
@@ -1,7 +1,10 @@
+import json
+
 class DBManager:
     # TODO
     def get_user_quest_ids(self, user_id: str):
-        return ["quest_id", "quest_id"]
+        authors = open_authors()
+        return authors[user_id]
 
     def get_quest_xml(self, quest_id: str):
         # While we don't have DB
@@ -9,8 +12,29 @@ class DBManager:
         with open(file_path) as f:
             return f.read()
 
+
     def save_quest(self, user_id: str, quest_id: str):
+        authors = open_authors()
+        if user_id not in authors.keys():
+            authors[user_id] = []
+        authors[user_id].append(quest_id)
+        close_authors()
         pass
+
+    # def open_quests():
+    #     with open('quests.json') as f:
+    #         quests = json.load(f)
+    #     return quests
+
+    def open_authors():
+        with open('authors.json') as f:
+            authors = json.load(f)
+        return authors
+
+    def close_authors():
+        with open('authors.json') as f:
+            authors = json.dump(f)
+        return
 
 
 db_manager = DBManager()
