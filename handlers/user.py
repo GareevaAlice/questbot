2387ccdb1770eb71dad435c3e5479540d98ac9ee
GareevaAlice <alica.gareeva@gmail.com>
2022-12-03 14:31:47 +0300

    stupid db

--- handlers/user.py
+++ handlers/user.py
@@ -14,7 +14,9 @@ with open("message_templates.json", "r") as f:
 
 @router.message(Command(commands=["user_info"]))
 async def user_info(message: Message):
+    quest_ids = db_manager.get_user_quest_ids(user_id=message.from_user.id)
     await message.answer(
         text=temp_text['user_info'].format(
-            quests=db_manager.get_user_quest_ids(user_id=message.from_user.id)),
+            quest_ids="\n\n".join([f"`{quest_id}`" for quest_id in quest_ids])),
+        parse_mode="MarkdownV2"
     )
--- utils/DBManager.py
+++ utils/DBManager.py
@@ -1,19 +1,23 @@
+from collections import defaultdict
+from typing import List
+
+
 class DBManager:
-    # TODO
-    def get_user_quest_ids(self, user_id: str):
-        return ["quest_id", "quest_id"]
+    stupid_db = defaultdict(list)
+
+    def get_user_quest_ids(self, user_id: str) -> List[str]:
+        return self.stupid_db[user_id]
 
     def get_quest_xml(self, quest_id: str):
-        # While we don't have DB
         try:
             file_path = f"/tmp/{quest_id}.xml"
             with open(file_path) as f:
                 return f.read()
-        except:
+        except Exception:
             raise ValueError
 
     def save_quest(self, user_id: str, quest_id: str):
-        pass
+        self.stupid_db[user_id].append(quest_id)
 
 
 db_manager = DBManager()
